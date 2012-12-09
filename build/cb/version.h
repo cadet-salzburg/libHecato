01bb96a190033040999fdd07e1a7052493897da7
otto <ottona@neotokio-base.localdomain>
2012-12-09 01:43:29 +0100

    - added CodeBlocks targets for shared object builds under linux

--- build/cb/version.h
+++ build/cb/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "06";
+	static const char DATE[] = "07";
 	static const char MONTH[] = "12";
 	static const char YEAR[] = "2012";
 	static const char UBUNTU_VERSION_STYLE[] = "12.12";
@@ -14,16 +14,16 @@ namespace AutoVersion{
 	static const char STATUS_SHORT[] = "b";
 	
 	//Standard Version Type
-	static const long MAJOR = 1;
-	static const long MINOR = 0;
+	static const long MAJOR = 0;
+	static const long MINOR = 5;
 	static const long BUILD = 31;
 	static const long REVISION = 40;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 30;
-	#define RC_FILEVERSION 1,0,31,40
-	#define RC_FILEVERSION_STRING "1, 0, 31, 40\0"
-	static const char FULLVERSION_STRING[] = "1.0.31.40";
+	#define RC_FILEVERSION 0,5,31,40
+	#define RC_FILEVERSION_STRING "0, 5, 31, 40\0"
+	static const char FULLVERSION_STRING[] = "0.5.31.40";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 31;
